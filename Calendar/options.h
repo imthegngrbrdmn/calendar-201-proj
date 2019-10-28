88f6fb11e85631ecfe721f3f00726adad2907e14
Millard A. Arnold V <maarnold7@alaska.edu>
2019-10-27 19:46:38 -0800

    Added functions to options.h, needs comments still.

--- Calendar/options.h
+++ Calendar/options.h
@@ -1,1 +1,15 @@
 #pragma once
+#include <iostream>
+#include <string>
+#include <vector>
+#include <ctime>
+
+struct event
+{
+	int date;
+	int time;
+	std::string title;
+}
+
+void help(std::vector<string>& printlist);
+std::vector<event> listEvents();
