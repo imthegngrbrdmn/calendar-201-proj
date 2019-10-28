d95dbdd5c207adee9000f3620ca1a500ad5a7919
Millard A. Arnold V <maarnold7@alaska.edu>
2019-10-27 20:17:13 -0800

    Added comments documenting the help function

--- Calendar/options.h
+++ Calendar/options.h
@@ -11,5 +11,10 @@ struct event
 	std::string title;
 }
 
+/*
+input: vector of strings that will be printed by main
+output: none (but vector is passed by reference so that it can be changed
+the help function will give a list of options which the user can use in order to get what they want.
+*/
 void help(std::vector<string>& printlist);
 std::vector<event> listEvents();
