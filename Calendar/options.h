aaaf487da57f2547d638f139d50174e913d7cb29
Millard A. Arnold V <maarnold7@alaska.edu>
2019-10-27 20:18:10 -0800

    Added std:: where it was missing so that the function will run.

--- Calendar/options.h
+++ Calendar/options.h
@@ -16,5 +16,5 @@ input: vector of strings that will be printed by main
 output: none (but vector is passed by reference so that it can be changed
 the help function will give a list of options which the user can use in order to get what they want.
 */
-void help(std::vector<string>& printlist);
+void help(std::vector<std::string>& printlist);
 std::vector<event> listEvents();
