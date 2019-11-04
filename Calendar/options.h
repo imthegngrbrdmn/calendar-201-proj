7da734aabedf0350e44b0ba07ffadf4fa4a3d129
maarnold7 <maarnold7@alaska.edu>
2019-11-04 09:38:31 -0900

    Added eventRange(), commented needs for listEvents & eventRange()

--- Calendar/options.h
+++ Calendar/options.h
@@ -12,9 +12,22 @@ struct event
 };
 
 /*
-input: vector of strings that will be printed by main
-output: none (but vector is passed by reference so that it can be changed
-the help function will give a list of options which the user can use in order to get what they want.
+	Input: vector of strings that will be printed by main
+	Output: none (but vector is passed by reference so that it can be changed
+The help function will give a list of options which the user can use in order to get what they want.
 */
-void help(std::vector<std::string>& printlist);
-std::vector<event> listEvents();
+void help(std::vector<std::string>& optionlist);
+
+/*
+	Input: vector of events to be manipulated later
+	Output: vector passed by reference will be changed
+The listEvents function will give a list of all events.
+*/
+void listEvents(std::vector<event>& events);
+
+/*
+	Input: vector of events to be manipulated later, int date to start, int date to end
+	Output: vector passed by reference will be changed
+The eventRange function will take a list of events and return all the events that are happening between those dates.
+*/
+void eventRange(std::vector<event>& events,int beginDate,int endDate);
