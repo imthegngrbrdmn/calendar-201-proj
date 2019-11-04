230d4fea6ac453d14bdff004ead55da5d97ebefc
maarnold7 <maarnold7@alaska.edu>
2019-11-04 09:46:21 -0900

    implemented listEvents() & eventRange() in order to make build succeed.

---
+++ Calendar/options.cpp
@@ -0,0 +1,17 @@
+#include"options.h"
+void help(std::vector<std::string>& optionlist)
+{
+	optionlist.push_back("Help");
+	optionlist.push_back("ListEvents");
+	optionlist.push_back("EventRange -begin_end");
+}
+
+void listEvents(std::vector<event>& events)
+{
+	//FIX ME: make me do stuff
+}
+
+void eventRange(std::vector<event>& events, int beginDate, int endDate)
+{
+	//FIX ME: make me do stuff
+}
